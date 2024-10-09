ee733e2e21216fb7e724aca4bebc1aec48f5ad92
kanuengnit-BB <149050395+LiquidCat-LC@users.noreply.github.com>
2024-10-09 16:02:12 +0700

    Add shaker

---
+++ Assets/scripts/Shaker.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shaker : MonoBehaviour
+{
+    public Image barImage; // ลิงก์กับ Image ที่เป็นหลอด bar
+    private float fillAmount = 0f;
+    private bool isShaking = false;
+
+    void Update()
+    {
+        // ตรวจสอบการเขย่า
+        if (Input.acceleration.magnitude > 2.0f)
+        {
+            if (!isShaking)
+            {
+                isShaking = true; // เริ่มเขย่า
+                StartCoroutine(Shake());
+            }
+        }
+        else
+        {
+            isShaking = false; // หยุดเขย่า
+        }
+    }
+
+    private System.Collections.IEnumerator Shake()
+    {
+        while (fillAmount < 1f && isShaking)
+        {
+            fillAmount += Time.deltaTime * 1f; // ปรับความเร็วในการเติม
+            barImage.fillAmount = fillAmount; // เติม bar
+            yield return null;
+        }
+    }
+}
