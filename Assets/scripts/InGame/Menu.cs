bafbb97f397033ace301382b5a8a28dd728c61ce
nicemare61 <kittipoj.meer@bumail.net>
2024-10-28 00:32:23 +0700

    AssignOrder1

--- Assets/scripts/InGame/Menu.cs
+++ Assets/scripts/InGame/Menu.cs
@@ -1,29 +1,34 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = System.Random;
 
 public class Menu : MonoBehaviour
 {
-    public int orders = 5;
+    public static int orD = 3;
+    public Orders[] ordersMenu = new Orders[orD];
     public string[] mainDishes = new string[] {"Rice","Fried Rice","Rice Berry" };
     public string[] sideDishes = new string[] {"French", "Fried Chicken","Grilled Shrimp" };
     public string[] curries = new string[] {"Green Curry", "Phanaeng", "Southern Curry" };
     Random random = new Random();
     
+    public class Orders
+    {
+        public string mainDish;
+        public string sideDish;
+        public string curry;
+    }
+  
     
     // Start is called before the first frame update
     void Start()
     {
-        for (int a = 0; a < orders; a++)
+        for (int a = 0; a < orD; a++)
         {
-            int r1 = random.Next(mainDishes.Length-1);
-            int r2 = random.Next(sideDishes.Length-1);
-            int r3 = random.Next(curries.Length-1);
-            Debug.Log(mainDishes[r1]);
-            Debug.Log(sideDishes[r2]);
-            Debug.Log(curries[r3]);
+            ordersMenu[a] = new Orders(){mainDish = mainDishes[random.Next(mainDishes.Length-1)], 
+                sideDish = sideDishes[random.Next(sideDishes.Length-1)], curry = curries[random.Next(curries.Length-1)]};
         }
     }
 
@@ -32,4 +37,10 @@ public class Menu : MonoBehaviour
     {
         
     }
+
+    public void CallOrders(Orders[] orders)
+    {
+        
+    }
+    
 }
